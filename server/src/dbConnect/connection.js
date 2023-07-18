ff6f36048e88e7010f53afda4564150307d13a55
Saroj <sarojt7475@gmail.com>
2023-07-18 17:00:08 +0545

    Resolved server-side & DB connection issues

--- server/src/dbConnect/connection.js
+++ server/src/dbConnect/connection.js
@@ -4,17 +4,22 @@ const mongoose = require('mongoose');
 const dbConfig = require('../config/dbConfig.json')
 
 
+// console.log(`${dbConfig.MONGODB_CONNECTION_URI}/${dbConfig.MONGODB_DATABASE_NAME}`);
+
+
 const connection = async() => {
     try{
-      await console.log(`${dbConfig.MONGODB_DATABASE_NAME}/${dbConfig.MONGODB_DATABASE_NAME}`)
+      // await console.log(`${dbConfig.MONGODB_DATABASE_NAME}/${dbConfig.MONGODB_DATABASE_NAME}`)
       const res =  await  mongoose.connect(`${dbConfig.MONGODB_CONNECTION_URI}/${dbConfig.MONGODB_DATABASE_NAME}`);
-      if(res) console.log("connected to mongodb")
-    }catch(err){
+      if(res) console.log("connected to mongodb");
+    }catch (err){
         console.log(err)
     }
 }
 
-module.exports = connection
+module.exports = connection;
+
+
 
 
 
--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@
 
 const express = require('express')
 require('dotenv').config()
-const connection = require('./db/connection')
+const connection = require('./dbConnect/connection')
 const Users = require('./models/users')
 connection()
 const app = express()
@@ -12,56 +12,29 @@ const port = process.env.PORT
 app.use(express.json())
 
 
-app.post('/register', async(req, res) => {
-  await Users.create(req.body)
-  res.json({
-    msg: "Cpmgrats, you are successfully registered!"
-  })
-})
+// app.post('/register', async(req, res) => {
+//   await Users.create(req.body)
+//   res.json({
+//     msg: "Cpmgrats, you are successfully registered!"
+//   })
+// })
 
+// Post methods
+app.post('/register', async (req, res) => {
+  await Products.create(req.body)
+  res.json({
+  msg: "Congrats, you are successfully registered!"
+  })
+ })
+  
 
 
  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Your app is listening on port ${port}`)
   })
  
 
 
-// Post methods
-app.post('/products', (req, res) => {
- Products.create(req.body)
- res.json({
- msg: "products"
- })
-})
- 
 
-
-
-// app.get('/products', async(req, res) => {
-//  const data = await Products.find()
-// })
- 
-// app.put('/products/:id', async(req, res) => {
-//  await Products.findByIdAndUpdate(req.params.id, req.body)
-//  })
- 
-//  app.delete('/products/:id', async(req, res) => {
-//  await Products.findByIdAndDelete(req.params.id)
-//  })
- 
-//  app.get('/products', async(req, res) => {
-//  const data = await Products.find()
-//  })
-
-
-
-//  app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-//   })
- 
-
-//   require('dotenv').config()
-//   console.log(process.env.port) //
 
 
